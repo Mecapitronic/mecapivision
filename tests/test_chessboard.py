659e5253edd21fe129b20ebd867c299cc883f2d9
PercevalSA <perceval.arenou@gmail.com>
2025-02-17 17:13:52 +0000

    fix unit test

--- tests/test_chessboard.py
+++ tests/test_chessboard.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from cv2 import imread
+import cv2 as cv
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
@@ -10,8 +10,15 @@ from chessboard import detect_corners
 
 class TestChessboard:
     def test_detect_corners(self):
+        expected = cv.imread("images/test_chessboard.tiff")
+
         img = "images/chessboard.jpg"
         result = detect_corners(img)
 
-        expected = imread("images/test_chessboard.jpg")
-        assert result == expected
+        assert result.shape == expected.shape
+
+        difference = cv.subtract(result, expected)
+        b, g, r = cv.split(difference)
+        assert cv.countNonZero(b) == 0
+        assert cv.countNonZero(g) == 0
+        assert cv.countNonZero(r) == 0
